57c8febb2fa9a5ff6361f60913fe2fc6dc52dbc7
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-20 17:45:19 +0200

    Create moving-turning.js

---
+++ moving-turning.js
@@ -0,0 +1,78 @@
+const { Board, Led, Motor } = require('johnny-five');
+const board = new Board();
+
+board.on('ready', () => {
+  // Define the GPIO pins for the wheels
+  const leftWheelPin1 = new Led(17);
+  const leftWheelPin2 = new Led(18);
+  const rightWheelPin1 = new Led(27);
+  const rightWheelPin2 = new Led(22);
+
+  // Function to move forward
+  function moveForward() {
+    leftWheelPin1.on();
+    leftWheelPin2.off();
+    rightWheelPin1.on();
+    rightWheelPin2.off();
+  }
+
+  // Function to move backward
+  function moveBackward() {
+    leftWheelPin1.off();
+    leftWheelPin2.on();
+    rightWheelPin1.off();
+    rightWheelPin2.on();
+  }
+
+  // Function to make a left turn
+  function turnLeft() {
+    leftWheelPin1.off();
+    leftWheelPin2.on();
+    rightWheelPin1.on();
+    rightWheelPin2.off();
+  }
+
+  // Function to make a right turn
+  function turnRight() {
+    leftWheelPin1.on();
+    leftWheelPin2.off();
+    rightWheelPin1.off();
+    rightWheelPin2.on();
+  }
+
+  // Function to stop
+  function stop() {
+    leftWheelPin1.off();
+    leftWheelPin2.off();
+    rightWheelPin1.off();
+    rightWheelPin2.off();
+  }
+
+  moveForward();
+  board.wait(2000, () => {
+    stop();
+    board.wait(1000, () => {
+      turnLeft();
+      board.wait(1000, () => {
+        stop();
+        board.wait(1000, () => {
+          turnRight();
+          board.wait(1000, () => {
+            stop();
+            board.wait(1000, () => {
+              moveBackward();
+              board.wait(2000, () => {
+                stop();
+                board.wait(1000, () => {
+                  // Cleanup GPIO pins
+                  board.io.reset();
+                  process.exit(0);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
