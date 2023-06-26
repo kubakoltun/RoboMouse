1e626a1a3b8ab37646d9f0bc7a277e99869d46fa
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-26 18:44:52 +0200

    Update distance_measurement.py

--- distance_measurement.py
+++ distance_measurement.py
@@ -4,20 +4,22 @@ import time
 
 GPIO.setmode(gpio.BCM)
 GPIO.setwarnings(False)
-TRIG = 23
-ECHO = 24
+TRIGEVEN = 2;
+ECHOEVEN = 4;
+TRIGODD = 1;
+ECHOODD = 3;
 print("Distance mesurement is in progress...")
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-GPIO.output(TRIG, False)
+GPIO.setup(TRIGEVEN, GPIO.OUT)
+GPIO.setup(ECHOEVEN, GPIO.IN)
+GPIO.output(TRIGEVEN, False)
 print("Sensor settles")
 time.sleep(2)
-GPIO.output(TRIG, True)
+GPIO.output(TRIGEVEN, True)
 time.sleep(0.00001)
-GPIO.output(TRIG, False)
-while GPIO.input(ECHO) == 0:
+GPIO.output(TRIGEVEN, False)
+while GPIO.input(ECHOEVEN) == 0:
 	pulse_start = time.time()
-while GPIO.input(ECHO) == 1:
+while GPIO.input(ECHOEVEN) == 1:
 	pulse_end = time.time()
 pulse_duration = pulse_end - pulse_start
 distance = pulse_duration * 17150
