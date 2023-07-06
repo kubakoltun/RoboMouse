bd7b7fe78549120f4778ca1b1b67ff05ec38e25e
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-06 19:44:00 +0200

    Update distance_measurement.js

--- js/distance_measurement.js
+++ js/distance_measurement.js
@@ -1,28 +1,26 @@
 const pigpio = require('pigpio-client')().then((pi) => {
-  const TRIGEVEN = 29;
-  const ECHOEVEN = 31;
-  const TRIGODD = 33;
-  const ECHOODD = 35;
+  const trigRight = 5
+  const echoRight = 6
 
-  pi.gpio(TRIGEVEN).modeSet('output');
-  pi.gpio(ECHOEVEN).modeSet('input');
+  pi.gpio(trigRight).modeSet('output');
+  pi.gpio(echoRight).modeSet('input');
 
   console.log('Distance measurement is in progress...');
 
-  pi.gpio(TRIGEVEN).digitalWrite(0);
+  pi.gpio(trigRight).digitalWrite(0);
   console.log('Sensor settles');
   setTimeout(() => {
-    pi.gpio(TRIGEVEN).digitalWrite(1);
+    pi.gpio(trigRight).digitalWrite(1);
     setTimeout(() => {
-      pi.gpio(TRIGEVEN).digitalWrite(0);
+      pi.gpio(trigRight).digitalWrite(0);
 
       let pulseStart;
-      while (pi.gpio(ECHOEVEN).digitalRead() === 0) {
+      while (pi.gpio(echoRight).digitalRead() === 0) {
         pulseStart = process.hrtime.bigint();
       }
 
       let pulseEnd;
-      while (pi.gpio(ECHOEVEN).digitalRead() === 1) {
+      while (pi.gpio(echoRight).digitalRead() === 1) {
         pulseEnd = process.hrtime.bigint();
       }
 
