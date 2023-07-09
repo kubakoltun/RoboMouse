c458860e684fa5cb18359cc2b3fb0a00d7675699
kubakoltun <kubakoltun1232@gmail.com>
2023-07-09 16:54:15 +0200

    another motor added to a board test

--- py/board_test.py
+++ py/board_test.py
@@ -4,22 +4,24 @@ import time
 # right wheel
 in1A = 8
 in2A = 23
-#enA = 3
-
+# left wheel
+in3B = 17
+in4B = 27
 
 def setup():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(in1A, GPIO.OUT)
 	GPIO.setup(in2A, GPIO.OUT)
-	#GPIO.setup(enA, GPIO.OUT)
+	GPIO.setup(in3B, GPIO.OUT)
+	GPIO.setup(in4B, GPIO.OUT)
 	
 	
 def forward(st):
     setup()
     GPIO.output(in1A, GPIO.HIGH)
     GPIO.output(in2A, GPIO.LOW)
-    #GPIO.output(in1A, False)
-    #GPIO.output(in2A, True)
+    GPIO.output(in3B, GPIO.HIGH)
+    GPIO.output(in4B, GPIO.LOW)
     time.sleep(st)
 	
 
