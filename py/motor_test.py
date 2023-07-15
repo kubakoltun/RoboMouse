57a160125b7523ba1b1f52ea7710c43a9bcc1126
kubakoltun <kubakoltun1232@gmail.com>
2023-07-15 11:55:47 +0200

    fixed the pwm pinout

--- py/motor_test.py
+++ py/motor_test.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 # right wheel
-in1A = 8
+in1A = 25
 in2A = 23
-enA = 3
+enA = 12
 # left wheel
 in3B = 17
 in4B = 27
-enB = 22
+enB = 13
 # note for the configuration, power the motors with battery,
 # remove the logic jumper, power the logic terminal with pi 5V
 # now it should be able to handle both en pins with or without a jumpers
@@ -19,28 +19,30 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1A, GPIO.OUT)
 GPIO.setup(in2A, GPIO.OUT)
 GPIO.setup(enA, GPIO.OUT)
-GPIO.output(in1A, GPIO.LOW)
-GPIO.output(in2A, GPIO.LOW)
+#GPIO.output(in1A, GPIO.LOW)
+#GPIO.output(in2A, GPIO.LOW)
 
 GPIO.setup(in3B, GPIO.OUT)
 GPIO.setup(in4B, GPIO.OUT)
 GPIO.setup(enB, GPIO.OUT)
-GPIO.output(in3B, GPIO.LOW)
-GPIO.output(in4B, GPIO.LOW)
+#GPIO.output(in3B, GPIO.LOW)
+#GPIO.output(in4B, GPIO.LOW)
 
 pA = GPIO.PWM(enA, 1000)
-pA.start(25)
+pA.start(75)
 pB = GPIO.PWM(enB, 1000)
-pB.start(25)
+pB.start(75)
 
 
 def move_forward():
+    #pA.start(50)
+    #pB.start(50)
     GPIO.output(in1A, GPIO.HIGH)
     GPIO.output(in2A, GPIO.LOW)
-    GPIO.output(enA, GPIO.HIGH)
+    #GPIO.output(enA, GPIO.HIGH)
     GPIO.output(in3B, GPIO.HIGH)
     GPIO.output(in4B, GPIO.LOW)
-    GPIO.output(enB, GPIO.HIGH)
+    #GPIO.output(enB, GPIO.HIGH)
 
 
 def move_backward():
