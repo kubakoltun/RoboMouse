52e47e179a5f943ff19e09330dfcc2213b249be0
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-09-04 22:31:57 +0200

    Update clean_selected.py

--- py/clean_selected.py
+++ py/clean_selected.py
@@ -1,12 +1,8 @@
 import RPi.GPIO as GPIO
 
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-
-pin_number = 17
-
-
+pin_number = int(input("Enter the GPIO pin number: ")) 
 GPIO.setup(pin_number, GPIO.IN)
 GPIO.cleanup()
